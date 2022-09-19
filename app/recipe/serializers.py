96ae00d16800b0798d2e02ff597b228fe9c67140
hito <hitonari@gmail.com>
2022-09-19 12:59:54 +0700

    fix lint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,6 +6,7 @@ from rest_framework import serializers
 
 from core.models import (Recipe)
 
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
@@ -14,9 +15,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
 
+
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
-
